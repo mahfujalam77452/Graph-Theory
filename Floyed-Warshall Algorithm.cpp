b4e93a0eb482f3e92979ad27c47d32a30f36a188
Mahfuj_SUST <107198635+mahfujalam77452@users.noreply.github.com>
2023-10-25 20:27:01 +0600

    Update Floyed-Warshall Algorithm.cpp

--- Floyed-Warshall Algorithm.cpp
+++ Floyed-Warshall Algorithm.cpp
@@ -3,16 +3,16 @@ using namespace std;
 const int INF=1e9+10;
 const int N=10000;
 int dist[N][N];
-int next[N][N]={0};//This 2D array is for retriving shortest path.
+int Next[N][N]={0};//This 2D array is for retriving shortest path.
 void Find_path(int u,int v){
     //Function for printing the sortest path between u and v.
-    if(next[u][v]==-1){
+    if(Next[u][v]==-1){
         cout<<"No path exist!"<<endl;
     }
      vector<int>path;
      path.push_back(u);
      while(u!=v){
-         u=next[u][v];
+         u=Next[u][v];
          path .push_back(u);
      }
      for(int i=0;i<path.size();i++)
@@ -39,8 +39,8 @@ int main()
         for(int j=0;j<N;j++)
         {
             //storing the initial next sortest element of each vertex.
-            if(dist[i][j]==INF)next[i][j]=-1;
-            else next[i][j]=j;
+            if(dist[i][j]==INF)Next[i][j]=-1;
+            else Next[i][j]=j;
         }
     }
     for(int k=1;k<=n;k++){
@@ -48,7 +48,7 @@ int main()
             for(int j=1;j<=n;j++){
                if(dist[i][k]!=INF&&dist[k][j]!=INF){
                     dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
-                    next[i][j]=next[i][k];
+                    Next[i][j]=Next[i][k];
                }
             }
         }
