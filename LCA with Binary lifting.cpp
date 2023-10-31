0dcd84ffcc766d106de0eea2a6d8061e0e1bc4e3
Mahfuj_SUST <107198635+mahfujalam77452@users.noreply.github.com>
2023-10-31 10:14:23 +0600

    Create LCA with Binary lifting.cpp

---
+++ LCA with Binary lifting.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int  MAX_N=10000;
+const int LOG=14;
+vector<int>adj[MAX_N];
+int up[MAX_N][LOG];
+vector<int>depth(MAX_N);
+void dfs(int a){
+    for(int b:adj[a]){
+        depth[b]=depth[a]+1;
+        up[b][0]=a;//a is the parent of b.
+        for(int j=1;j<LOG;j++){
+            up[b][j]=up[up[b][j-1]][j-1];
+        }
+        dfs(b);
+    }
+}
+int get_lca(int a,int b){
+    if(depth[a]<depth[b])
+                    swap(a,b);
+    int k=depth[a]-depth[b];
+    for(int j=LOG-1;j>=0;j--){
+        if(k&(1<<j)){
+            a=up[a][j];
+        }
+    }
+    //We get here node a and b with similer depth.
+    if(a==b){
+        return a;
+    }
+    for(int j=LOG-1;j>=0;j--){
+        if(up[a][j]!=up[b][j]){
+            a=up[a][j];
+            b=up[b][j];
+        }
+    }
+    return up[a][0];
+}
+int main()
+{
+    int n;
+	cin >> n;
+	for(int v = 0; v < n; ++v) {
+		// read children of v
+		int cnt;
+		cin >> cnt;
+		for(int i = 0; i < cnt; i++) {
+			int c;
+			cin >> c;
+			adj[v].push_back(c);
+		}
+	}
+	dfs(0);
+	int q;
+	cin >> q;
+	for(int i = 0; i < q; i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << get_lca(a, b) << "\n";
+	}
+    
+    return 0;
+}
